ee0e290a84f3ffb0c57771471fad65e8910d27c6
Devin Hight <dhightnm@gmail.com>
2021-08-31 17:48:46 -0600

    added one more test

--- SinglyLinkedList/Singly_Linked_Lists.test.js
+++ SinglyLinkedList/Singly_Linked_Lists.test.js
@@ -7,6 +7,11 @@ test('should return a basic object', () => {
   expect(typeof testObj === 'object').toBe(true);
 });
 
-test('2 + 2 is equal ot four', () => {
+test('Should push a new value to the created class', () => {
+  testObj.push('value1');
+  expect(testObj.get(0).value).toBe('value1');
+});
+
+test('2 + 2 is equal to 4', () => {
   expect(2 + 2).toEqual(4);
 });
