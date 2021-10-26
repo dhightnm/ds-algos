90658422f3c868e5f30085c473bffdba51fdfe37
Devin Hight <dhightnm@gmail.com>
2021-10-26 15:25:43 -0700

    Added Get

--- HashTable/hashTable.js
+++ HashTable/hashTable.js
@@ -13,19 +13,50 @@
   return total;
 }; */
 
-const hash = (key, arrayLen) => {
-  let total = 0;
-  const PRIME = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    const char = key[i];
-    const value = char.charCodeAt(0) - 96;
-    total = (total * PRIME + value) % arrayLen;
-  }
-  return total;
-};
-
 /* Using seperate chaning you store collisions in the same spot
-   using nested data structures */
+using nested data structures */
 
-/* Linear probing you search throught the entire array to look for 
-   the next available slot */
+/* Linear probing you search throught the entire array to look for
+the next available slot */
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  hash(key) {
+    let total = 0;
+    const WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const char = key[i];
+      const value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    const hashed = this.hash(key);
+    if (!this.keyMap[hashed]) {
+      this.keyMap[hashed] = [];
+    }
+    this.keyMap[hashed].push([key, value]);
+    return hashed;
+  }
+
+  get(key) {
+    const hashed = this.hash(key);
+    if (this.keyMap[hashed]) {
+      for (let i = 0; i < this.keyMap[hashed].length; i++) {
+        if (this.keyMap[hashed][i][0] === key) {
+          return this.keyMap[hashed][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
+
+const ht = new HashTable(17);
+
+ht.set('hello world', 'firstHash');
